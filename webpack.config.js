7b5cf47b953afac019bf0aa8cc8fed20a2bd148d
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-02-07 23:32:19 +0300

    dev & prod mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,55 @@
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require("copy-webpack-plugin")
-const path = require('path')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: {
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production'
+  const isDev = !isProd
+
+  console.log(isProd, 'Prod')
+  console.log(isDev, 'Dev')
+
+  const filename = ext => isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`
+  return {
+    context: path.resolve(__dirname, 'src'),
+      entry: {
     main: [
       'core-js/stable',
       'regenerator-runtime/runtime',
       './index.js'
     ]
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
-  },
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src', 'core'),
-    }
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src', 'favicon.ico'),
-          to: path.resolve(__dirname, 'dist')
-        },
-      ],
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].bundle.css'
-    })
-  ],
-  module: {
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+        filename: filename('js'),
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.js'],
+        alias: {
+        '@': path.resolve(__dirname, 'src'),
+          '@core': path.resolve(__dirname, 'src', 'core'),
+      }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src', 'favicon.ico'),
+            to: path.resolve(__dirname, 'dist')
+          },
+        ],
+      }),
+      new MiniCssExtractPlugin({
+        filename: filename('css')
+      })
+    ],
+    devtool: isDev ? 'source-map' : false,
+      module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
@@ -61,4 +71,5 @@ module.exports = {
       }
     ],
   },
+  }
 }
