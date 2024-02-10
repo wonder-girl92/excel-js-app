d75c06d2161701f6a46e8f5e5f5b263fbe7b9a27
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-02-11 02:29:39 +0300

    eslint-webpack-plugin, rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require("copy-webpack-plugin")
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const ESLintPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
 module.exports = (env, argv) => {
   const isProd = argv.mode === 'production'
@@ -11,10 +11,12 @@ module.exports = (env, argv) => {
   console.log(isProd, 'Prod')
   console.log(isDev, 'Dev')
 
-  const filename = ext => isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`
+  const filename = (ext) => isProd ?
+    `[name].[contenthash].bundle.${ext}` :
+    `[name].bundle.${ext}`
 
   const plugins = () => {
-    const base =  [
+    const base = [
       new HtmlWebpackPlugin({
         template: './index.html'
       }),
@@ -40,13 +42,13 @@ module.exports = (env, argv) => {
   return {
     target: 'web',
     context: path.resolve(__dirname, 'src'),
-      entry: {
-    main: [
-      'core-js/stable',
-      'regenerator-runtime/runtime',
-      './index.js'
-    ]
-  },
+    entry: {
+      main: [
+        'core-js/stable',
+        'regenerator-runtime/runtime',
+        './index.js'
+      ]
+    },
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: filename('js'),
@@ -54,9 +56,9 @@ module.exports = (env, argv) => {
     },
     resolve: {
       extensions: ['.js'],
-        alias: {
+      alias: {
         '@': path.resolve(__dirname, 'src'),
-          '@core': path.resolve(__dirname, 'src', 'core'),
+        '@core': path.resolve(__dirname, 'src', 'core'),
       }
     },
     devServer: {
@@ -67,27 +69,27 @@ module.exports = (env, argv) => {
     },
     devtool: isDev ? 'source-map' : false,
     plugins: plugins(),
-      module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env']
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
         }
-      }
-    ],
-  },
+      ],
+    },
   }
 }
