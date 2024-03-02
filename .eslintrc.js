e6baef724dd72806ee74afe01d8d607a05d4be32
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-03-03 01:11:33 +0300

    show up components in ui

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,22 @@
 module.exports = {
-  parser: "@babel/eslint-parser",
+  parser: '@babel/eslint-parser',
   parserOptions: {
     babelOptions: {
-      configFile: "./babel.config.json"
+      configFile: './babel.config.json'
     }
   },
-    env: {
-      browser: true,
-      node: true,
-      es6: true
-    },
-    extends: ['eslint:recommended', 'google'],
-    rules: {
-      'semi': 'off',
-      'comma-dangle': 'off',
-      'require-jsdoc': 'off'
-    }
-};
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'google'],
+  rules: {
+    'semi': 'off',
+    'comma-dangle': 'off',
+    'require-jsdoc': 'off',
+    // 'object-curly-spacing': 2,
+    // 'space-in-parens': 2,
+    // 'indent': 'off'
+  }
+}
