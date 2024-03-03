13da3b9e28423cdc07fa8ae46f554f9a2c5df936
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-03-04 01:57:23 +0300

    create elements

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,9 +14,6 @@ module.exports = {
   rules: {
     'semi': 'off',
     'comma-dangle': 'off',
-    'require-jsdoc': 'off',
-    // 'object-curly-spacing': 2,
-    // 'space-in-parens': 2,
-    // 'indent': 'off'
+    'require-jsdoc': 'off'
   }
 }
