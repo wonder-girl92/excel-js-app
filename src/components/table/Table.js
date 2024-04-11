8d938bb27504745987273db467bc5cfdf3a0abb9
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-04-11 23:58:54 +0300

    resize width columns

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,6 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from './table.template'
+import {$} from '../../core/dom'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -16,7 +17,16 @@ export class Table extends ExcelComponent {
 
   onMousedown(event) {
     if (event.target.dataset.resize) {
-      console.log('Start resizing', event.target.dataset.resize)
+      const $resizer = $(event.target)
+      const $parent = $resizer.closest('[data-type="resizable"]')
+      const coords = $parent.getCoords()
+
+      document.onmousemove = (e) => {
+        const delta = e.pageX - coords.right
+        const value = coords.width + delta
+
+        $parent.$el.style.width = value + 'px'
+      }
     }
   }
 }
