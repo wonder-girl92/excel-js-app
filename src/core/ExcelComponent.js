3805d556052b0579a5dac14edc4c4bcabd09210e
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-06-23 01:15:21 +0300

    subscribe/unsubscribe to events

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,10 +5,12 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners)
     this.name = options.name || ''
     this.emitter = options.emitter
+    this.unsubscribers = []
 
     this.prepare()
   }
 
+  // настройка компонента до init
   prepare() {
 
   }
@@ -18,11 +20,25 @@ export class ExcelComponent extends DomListener {
     return ''
   }
 
+  // уведомление слушателей про событие event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  // подписка на событие event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
+  }
+
+  // инициализация компонента, добавление DOM-слушателей
   init() {
     this.initDOMListeners()
   }
 
+  // удаление компонента, чистка слушателей
   destroy() {
     this.removeDOMListeners()
+    this.unsubscribers.forEach((unsub) => unsub())
   }
 }
