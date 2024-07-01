f6701341fbb6d79e0881db50fcc0900f28839a76
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-07-01 16:47:35 +0300

    store and ui communication

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -34,6 +34,9 @@ export class Table extends ExcelComponent {
     this.$on('formula:done', () => {
       this.selection.current.focus()
     })
+    this.$subscribe((state) => {
+      console.log('TableState', state)
+    })
   }
 
   selectCell($cell) {
@@ -51,7 +54,7 @@ export class Table extends ExcelComponent {
             .map((id) => this.$root.find(`[data-id="${id}"]`))
         this.selection.selectGroup($cells)
       } else {
-        this.selection.select($target)
+        this.selectCell($target)
       }
     }
   }
