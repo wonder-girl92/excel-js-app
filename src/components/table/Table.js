09857701b7cad948a132e97ceace19ae3433ef5f
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-07-02 13:20:52 +0300

    create col state

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -44,9 +44,19 @@ export class Table extends ExcelComponent {
     this.$emit('table:select', $cell)
   }
 
+  async resizeTable(event) {
+    try {
+      const data = await resizeHandler(this.$root, event)
+      this.$dispatch({type: 'TABLE_RESIZE', data})
+      console.log('resize data', data)
+    } catch (e) {
+      console.warn('Resize error', e.message)
+    }
+  }
+
   onMousedown(event) {
     if (shouldResize(event)) {
-      resizeHandler(this.$root, event)
+      this.resizeTable(event)
     } else if (isCell(event)) {
       const $target = $(event.target)
       if (event.shiftKey) {
