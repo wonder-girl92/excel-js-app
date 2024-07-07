6bca1c81fa3c0fe25b2439d7e9f7aae3bd88e57f
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-07-07 15:44:32 +0300

    line size

--- src/components/table/table.resize.js
+++ src/components/table/table.resize.js
@@ -43,7 +43,9 @@ export function resizeHandler($root, event) {
 
       resolve({
         value,
-        id: type === 'col' ? $parent.data.col : null
+        type,
+        // id: type === 'col' ? $parent.data.col : $parent.data.row
+        id: $parent.data[type]
       })
 
       $resizer.css({
