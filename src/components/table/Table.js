02980710ce416f5f82ba7e58571fceefd210e9ca
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-07-10 12:39:15 +0300

    cell's state save

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -31,6 +31,7 @@ export class Table extends ExcelComponent {
 
     this.$on('formula:input', (text) => {
       this.selection.current.text(text)
+      this.updateTextInStore(text)
     })
     this.$on('formula:done', () => {
       this.selection.current.focus()
@@ -89,8 +90,15 @@ export class Table extends ExcelComponent {
     }
   }
 
+  updateTextInStore(value) {
+    this.$dispatch(actions.changeText({
+      id: this.selection.current.id(),
+      value: $(event.target).text()
+    }))
+  }
+
   onInput(event) {
-    this.$emit('table:input', $(event.target))
+    this.updateTextInStore($(event.target).text())
   }
 }
 
