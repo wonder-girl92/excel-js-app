0af49e9932448d4babf9d70de121ecfa991283b7
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-08-02 13:20:54 +0300

    Finish redux and table logic

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -34,6 +34,7 @@ function toCell(state, row) {
       data-col="${col}"
       data-type="cell"
       data-id="${id}"
+      data-value="${data || ''}"
       style="${styles}; width: ${width}"
     >${parse(data) || ''}</div>
     `
--- src/core/parse.js
+++ src/core/parse.js
@@ -4,6 +4,7 @@ export function parse(value = '') {
       return eval(value.slice(1))
     } catch (e) {
       return value
+      // console.warn('Skipping parse error', e.message)
     }
   }
   return value
