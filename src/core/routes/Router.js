550fcaccc48b11bad0d0bfd4bd0fc18c3a677586
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-08-10 03:22:46 +0300

    change page

--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,4 +1,5 @@
 import {$} from '../dom'
+import {ActiveRoute} from './ActiveRoute'
 
 export class Router {
   constructor(selector, routes) {
@@ -7,6 +8,9 @@ export class Router {
     }
     this.$placeholder = $(selector)
     this.routes = routes
+
+    this.page = null
+
     this.changePageHandler = this.changePageHandler.bind(this)
 
     this.init()
@@ -18,11 +22,21 @@ export class Router {
   }
 
   changePageHandler() {
-    const Page = this.routes.excel
-    const page = new Page()
-    this.$placeholder.append(page.getRoot())
+    if (this.page) {
+      this.page.destroy()
+    }
 
-    page.afterRender()
+    this.$placeholder.clear()
+
+    const Page = ActiveRoute.path.includes('excel')
+      ? this.routes.excel
+      : this.routes.dashboard
+
+    this.page = new Page(ActiveRoute.param)
+
+    this.$placeholder.append(this.page.getRoot())
+
+    this.page.afterRender()
   }
 
   destroy() {
