44c81523514419ef06543f4262441f5f04559369
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-08-10 04:29:44 +0300

    create main page

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,38 +1,25 @@
 import {Page} from '../core/Page'
 import {$} from '../core/dom'
+import {createRecordsTable} from './dashboard.functions'
 
 export class DashboardPage extends Page {
   getRoot() {
+    const now = Date.now().toString()
     return $.create('div', 'db').html(`
      <div class="db__header">
-            <h1>Excel Dashboard</h1>
+            <h1>Excel. Панель управления</h1>
         </div>
 
         <div class="db__new">
             <div class="db__view">
-                <a href="#" class="db__create">
+                <a href="#excel/${now}" class="db__create">
                     Новая <br/> Таблица
                 </a>
             </div>
         </div>
 
         <div class="db__table db__view">
-            <div class="db__list-header">
-                <span>Название</span>
-                <span>Дата открытия</span>
-            </div>
-
-            <ul class="db__list">
-                <li class="db__record">
-                    <a href="#">Таблица номер 1</a>
-                    <strong>25.02.2024</strong>
-                </li>
-                <li class="db__record">
-                    <a href="#">Таблица номер 2</a>
-                    <strong>25.02.2024</strong>
-                </li>
-            </ul>
-
+           ${createRecordsTable()}
         </div>
     `)
   }
---
+++ src/pages/dashboard.functions.js
@@ -0,0 +1,41 @@
+function toHTML() {
+  return `
+  <li class="db__record">
+      <a href="#">Таблица номер 1</a>
+      <strong>25.02.2024</strong>
+  </li>
+`
+}
+
+function getAllKeys() {
+  const keys = []
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i)
+    if (!key.includes('excel')) {
+      continue
+    }
+    keys.push(key)
+  }
+  return keys
+}
+
+export function createRecordsTable() {
+  const keys = getAllKeys()
+
+  if (!keys.length) {
+    return `
+  <p>Вы пока не создали ни одной таблицы</p>
+  `
+  }
+
+  return `
+ <div class="db__list-header">
+     <span>Название</span>
+     <span>Дата открытия</span>
+ </div>
+
+  <ul class="db__list">
+    ${keys.map(toHTML).join('')}
+  </ul>
+  `
+}
