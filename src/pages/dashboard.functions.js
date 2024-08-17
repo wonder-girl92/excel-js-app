4a680d00d7a09e9a92794f2c554985a26d8f5cf9
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-08-17 15:42:54 +0300

    new table opened date

--- src/pages/dashboard.functions.js
+++ src/pages/dashboard.functions.js
@@ -7,7 +7,10 @@ function toHTML(key) {
   return `
   <li class="db__record">
       <a href="#excel/${id}">${model.title}</a>
-      <strong>25.02.2024</strong>
+      <strong>
+        ${new Date(model.openedDate).toLocaleDateString()}
+        ${new Date(model.openedDate).toLocaleTimeString()}
+       </strong>
   </li>
 `
 }
