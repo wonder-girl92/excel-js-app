6d64baf1991fdc9b2c21452e4ca46c160c0c9351
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-08-18 19:16:34 +0300

    define plugin, prod&dev settings

--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -23,7 +23,6 @@ export class ExcelPage extends Page {
     const store = createStore(rootReducer, initialState)
 
     const stateListener = debounce((state) => {
-      console.log('App state: ', state)
       storage(storageName(params), state)
     }, 300)
 
@@ -38,7 +37,6 @@ export class ExcelPage extends Page {
   }
 
   afterRender() {
-    console.log('afterRender')
     this.excel.init()
   }
 
